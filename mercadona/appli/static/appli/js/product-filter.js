a8bda2aea9371a5702e2a7d1ee2836dcad576ecb
Yann N <yann.solidity@gmail.com>
2023-04-15 18:27:13 +0200

    filtres prix

--- mercadona/appli/static/appli/js/product-filter.js
+++ mercadona/appli/static/appli/js/product-filter.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-  $(".filter-checkbox").on('click', function(){
+  $(".filter-checkbox, #priceFilterBtn").on('click', function(){
     var _filterObj={};
+    var _minPrice=$('#minPrice').attr('min');
+    var _maxPrice=$('#maxPrice').val();
+    _filterObj.minPrice=_minPrice;
+		_filterObj.maxPrice=_maxPrice;
     $(".filter-checkbox").each(function(index,ele){
       var _filterVal= $(this).val()
       var _filterKey= $(this).data('filter');
@@ -10,7 +14,7 @@ $(document).ready(function(){
 
   });
   console.log(_filterObj);
-
+  console.log()
   //Run Ajax
   $.ajax({
     url: 'filter-data',
@@ -25,6 +29,20 @@ $(document).ready(function(){
     //  $(".ajaxLoader").hide();
     }
   });
+});
+
+$("#maxPrice").on('blur',function(){
+  var _min=$(this).attr('min');
+  var _max=$(this).attr('max');
+  var _value=$(this).val();
+  console.log(_value,_min,_max);
+  if(_value < parseInt(_min) || _value > parseInt(_max)){
+    alert('Valeurs devraient être entre :'+_min+' et '+_max);
+    $(this).val(_min);
+    $(this).focus();
+    $("#rangeInput").val(_min);
+    return false;
+  }
 });
 
 });
--- mercadona/appli/views.py
+++ mercadona/appli/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from .models import *
 from django.template.loader import render_to_string
-
+from django.db.models import Max, Min
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def index(request):
     template = loader.get_template('appli/index.html')
     context = {}
@@ -20,17 +21,71 @@ def catalogue(request):
     size_standard = SizeStandard.objects.all().order_by('display_order')
     size_pants = SizePant.objects.all().order_by('title')
     size_shoes = SizeShoe.objects.all().order_by('title')
+    gender_list = Gender.objects.all().order_by('title')
+    categorie_list = Category.objects.all().order_by('title')
+    minMaxPrice = data.aggregate(Min('product_price'),Max('product_price'))
     template = loader.get_template('appli/catalogue.html')
-    context = {"data": data, "size_standard": size_standard, "size_pants": size_pants, "size_shoes" : size_shoes }
+    context = {"data": data, "size_standard": size_standard, "size_pants": size_pants, "size_shoes" : size_shoes,
+    "gender_list":gender_list, 'categorie_list':categorie_list, "minMaxPrice":minMaxPrice}
     return HttpResponse(template.render(context,request))
 
 def filter_data(request):
     size_standard=request.GET.getlist('size_standard[]')
     size_pants=request.GET.getlist('size_pants[]')
     size_shoes=request.GET.getlist('size_shoes[]')
-    allProducts= Product.objects.all().order_by('-id').distinct()
-    if len(size_standard)>0 or len(size_pants)>0 or len(size_shoes)>0 :
+    categorie_list=request.GET.getlist('categorie_list[]')
+    gender_list=request.GET.getlist('gender_list[]')
+
+    maxPrice=request.GET['maxPrice']
+    
+    allProducts=Product.objects.all().order_by('-id').distinct()
+
+    allProducts=allProducts.filter(product_price__lte=maxPrice)
+    if len(size_standard)>0 or len(size_pants)>0 or len(size_shoes)>0:
        allProducts=allProducts.filter(product_size_standard__title__in = size_standard).distinct() | allProducts.filter(product_size_pants__title__in = size_pants).distinct() | allProducts.filter(product_size_shoes__title__in = size_shoes).distinct()
-            
-    t=render_to_string('appli/ajax-product-list.html', {'data':allProducts})
+    if len(categorie_list)>0:
+        allProducts = allProducts.filter(product_category__title__in = categorie_list).distinct()
+    if len(gender_list)>0:
+        allProducts=allProducts.filter(product_gender__title__in = gender_list).distinct()
+                    
+    
+    context ={'data':allProducts}        
+    t=render_to_string('appli/ajax-product-list.html', context)
     return JsonResponse({'data': t})
+
+
+
+class ViewPaginatorMixin(object):
+    min_limit = 1
+    max_limit = 10
+
+    def paginate(self, object_list, page=1, limit=10, **kwargs):
+        try:
+            page = int(page)
+            if page < 1:
+                page = 1
+        except (TypeError, ValueError):
+            page = 1
+
+        try:
+            limit = int(limit)
+            if limit < self.min_limit:
+                limit = self.min_limit
+            if limit > self.max_limit:
+                limit = self.max_limit
+        except (ValueError, TypeError):
+            limit = self.max_limit
+
+        paginator = Paginator(object_list, limit)
+        try:
+            objects = paginator.page(page)
+        except PageNotAnInteger:
+            objects = paginator.page(1)
+        except EmptyPage:
+            objects = paginator.page(paginator.num_pages)
+        data = {
+            'previous_page': objects.has_previous() and objects.previous_page_number() or None,
+            'next_page': objects.has_next() and objects.next_page_number() or None,
+            'data': list(objects)
+        }
+        return data
