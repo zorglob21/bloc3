47a8cc4810590c7c7099c7adbb59c6527a6e481a
Yann N <yann.solidity@gmail.com>
2023-04-21 17:12:27 +0200

    déploiement 4

--- mercadona/mercadona/settings.py
+++ mercadona/mercadona/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 #SECURE_SSL_REDIRECT= True
 
 # Application definition
