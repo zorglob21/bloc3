24d1a3d68f724fb88c79b5c8ad9ca284673d4fdc
Yann N <yann.solidity@gmail.com>
2023-05-16 10:29:34 +0200

    deploiement fly io 2

--- mercadona/settings.py
+++ mercadona/settings.py
@@ -112,7 +112,7 @@ elif DEBUG == False:
     }
 
     #those settings need to be changed if the application is running on locally simulated production environment
-    ALLOWED_HOSTS = ['yannn-bloc3.onrender.com']
+    ALLOWED_HOSTS = ['*']
     SECURE_SSL_REDIRECT= True
     
     INSTALLED_APPS = [
